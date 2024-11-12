f466c066675ee330066678f3f21ce536ffc38361
aradhya4 <aradhya.23b0131006@abes.ac.in>
2024-11-12 12:38:04 +0530

    Add files via upload

---
+++ 07.radius.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+    int main() {
+    float r,area;
+    printf("enter the radius of the circle: ");
+    scanf("%f",&r);
+
+    area= 2*r*r;
+    printf("The area of largest possible rectangle(square) is %f",area);
+}
+
